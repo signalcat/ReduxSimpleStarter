ecabe037e3205f37e9209d98bc0ce37c863cdbcd
He Zhang <hezhang757@gmail.com>
2017-02-26 17:29:07 -0800

    Automatically update searching result after type in serach bar.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyAsFQ_PosZtet8zGfyE1IXzTCJ8ZDjjzts';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//create a new component. This component should produce some html
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { 
+			videos: [],
+			selectedVideo: null 
+		};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY , term: term}, (videos) => {
+			this.setState({ 
+				videos: videos, 
+				selectedVideo: videos[0]
+			});
+		}); 
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+//Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
